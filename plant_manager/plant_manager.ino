d3090b2981519d486ee39d6b205ec60ea895a1c4
enicolodi <eduzyolo@gmail.com>
2024-01-10 22:32:39 +0100

    added voltage and percentage calculation

--- plant_manager/plant_manager.ino
+++ plant_manager/plant_manager.ino
@@ -1,3 +1,22 @@
+
+
+#define R1 1.000                  //First Resistence Voltage divider --> to adjust
+#define R2 1.000                  //Second Resistence Voltage divider --> to adjust
+#define VBAT_MAX 3100       // milliVolts - 1.5V * 2 AA - fully charged batteries
+#define VBAT_MIN 2800       // milliVolts - due to battery regulator, lower than this we consider the batteries discharged 
+
+float readVoltage() {                         //Function to compute the battery voltage
+  int reading = analogRead(A3);               //Analog Read of the battery voltage
+  float volt = map(reading, 0, 1023, 0, 330); //Mapping from AnalogRead to Volt
+  volt = (((R1 + R2) * volt ) / R2) / 100.0;  //Compute the battery voltage
+  return volt;
+}
+
+uint8_t getBatteryPercentage(){
+  float voltage = readVoltage()
+  uint8_t percentage_battery = 100.0 / (VBAT_MAX - VBAT_MIN) * 1.0 * (voltage*1000.0 - VBAT_MIN);
+}
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -5,5 +24,5 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
